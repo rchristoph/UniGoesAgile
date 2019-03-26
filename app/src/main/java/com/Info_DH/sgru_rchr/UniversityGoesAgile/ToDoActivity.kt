689af436d93ad624487ae41f603fe9242f7bca21
Sebastian-Gru <sebigruessinger@gmail.com>
2019-03-26 21:39:45 +0100

    Navigation_Drawer

--- app/src/main/java/com/Info_DH/sgru_rchr/UniversityGoesAgile/ToDoActivity.kt
+++ app/src/main/java/com/Info_DH/sgru_rchr/UniversityGoesAgile/ToDoActivity.kt
@@ -1,13 +1,19 @@
 package com.Info_DH.sgru_rchr.UniversityGoesAgile
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.v4.view.MenuItemCompat
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_navigationdrawer.*
+import kotlinx.android.synthetic.main.app_bar_navigationdrawer.*
+import android.content.Intent
+import android.support.v4.view.MenuItemCompat
+import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -15,10 +21,16 @@ import kotlinx.android.synthetic.main.activity_todo.*
 import kotlinx.android.synthetic.main.content_main.*
 import com.google.firebase.database.*
 import android.support.v7.widget.ShareActionProvider
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.nav_header_navigationdrawer.*
+import java.net.URL
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
 
 
 
-class ToDoActivity : AppCompatActivity(), TaskRowListener {
+
+class ToDoActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, TaskRowListener {
 
     lateinit var _db: DatabaseReference
     lateinit var _dbuser: DatabaseReference
@@ -33,12 +45,22 @@ class ToDoActivity : AppCompatActivity(), TaskRowListener {
 
     lateinit var _adapter: TaskAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todo)
+        setContentView(R.layout.activity_navigationdrawer)
+
         _dbprojekt = FirebaseDatabase.getInstance().getReference("Projects")
         _db = FirebaseDatabase.getInstance().getReference("tasks")
         _dbuser = FirebaseDatabase.getInstance().getReference("Names")
+        val imageView = findViewById<ImageView>(R.id.Profilbild)
+
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
 
         println("Die uid ist: $uid")
 
@@ -62,10 +84,14 @@ class ToDoActivity : AppCompatActivity(), TaskRowListener {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
             override fun onDataChange(snapshot: DataSnapshot) {
-                println("Der  Wert ist: ${snapshot.value}")
-                projektIdent = snapshot.value.toString()
+                println("Der  Wert ist: ${snapshot.child("ProjektId").value}")
+                projektIdent = snapshot.child("ProjektId").value.toString()
+                username.text = "${snapshot.child("Name").value.toString()}"
+                nickname.text = "${snapshot.child("nickName").value.toString()} (Nickname)"
+
+
                 println("Die Projektident vorm Funktionsstart ist: $projektIdent")
-                if (snapshot.value == null){
+                if (snapshot.child("ProjektId").value == null){
                     startChoose()
                 }
                 _dbprojekt.child(projektIdent).orderByKey().addValueEventListener(_taskListener)
@@ -75,7 +101,7 @@ class ToDoActivity : AppCompatActivity(), TaskRowListener {
         _taskList = mutableListOf<Task>()
 
         _adapter = TaskAdapter(this, _taskList!!)
-         listviewTask!!.setAdapter(_adapter)
+        listviewTask!!.setAdapter(_adapter)
 
 
         fab.setOnClickListener {view ->
@@ -87,42 +113,77 @@ class ToDoActivity : AppCompatActivity(), TaskRowListener {
         }
 
         //_db.orderByKey().addValueEventListener(_taskListener)
-        _dbuser.child(uid).child("ProjektId").addValueEventListener(_projectListener)
+        _dbuser.child(uid).addValueEventListener(_projectListener)
         println("Die Projektident nr ist diese hier: $projektIdent")
         println("das ist _dbproject: $_taskListener")
 
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+
+
+
+
+
+
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.navigationdrawer, menu)
 
         val shareItem = menu!!.findItem(R.id.menu_item_share)
 
         shareActionProvider = MenuItemCompat.getActionProvider(shareItem) as ShareActionProvider
 
+        // Inflate the menu; this adds items to the action bar if it is present.
+
         setShareIntent()
+
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         if(item!!.itemId == R.id.signOut)
         {
             mAuth.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
             Toast.makeText(this, "Logged out", Toast.LENGTH_LONG).show()
+            return super.onOptionsItemSelected(item)
         }
         else if (item!!.itemId == R.id.signoutofproject){
             startActivity(Intent(this, ChooseProject::class.java))
+            return super.onOptionsItemSelected(item)
 
         }
         else if (item!!.itemId == R.id.menu_item_share){
+            return super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
+
+        when (item.itemId) {
+            R.id.action_settings2 -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+
     }
 
-    //Funktion für das TEilen der Prokekt ID
+
     private fun setShareIntent() {
 
         val shareIntent = Intent(Intent.ACTION_SEND)
@@ -216,17 +277,45 @@ class ToDoActivity : AppCompatActivity(), TaskRowListener {
     }
 
     override fun onTaskDelete(objectId: String) {
-       // val task = _db.child(Statics.FIREBASE_TASK).child(objectId)
+        // val task = _db.child(Statics.FIREBASE_TASK).child(objectId)
         val task = _dbprojekt.child(projektIdent).child("tasks").child("task").child(objectId)
         task.removeValue()
         println("Das ist ist task: $task")
     }
     override fun onTaskEdit(objectId: String, taskDesc: String) {
         //hier nehme ich die Task-ID, von der aus ich in der naechsten Activity direkt auf die Childnodes zugreifen kann
-        val intent = Intent(this@ToDoActivity, EditTask::class.java)
+        val intent = Intent(this, EditTask::class.java)
         intent.putExtra("obID", objectId)
         intent.putExtra("tDesc", taskDesc)
         startActivity(intent)
     }
-}
 
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
